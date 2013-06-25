b8ad769a502e7edc281b37928fe3f271de2b74a4
Aaswad Satpute <excellentmathematics@gmail.com>
2013-06-25 21:33:00 +0530

    First Commit

---
+++ CricDeCode/src/co/acjs/cricdecode/MainActivity.java
@@ -0,0 +1,27 @@
+package co.acjs.cricdecode;
+
+import android.os.Bundle;
+
+import com.actionbarsherlock.app.SherlockFragmentActivity;
+import com.google.analytics.tracking.android.EasyTracker;
+
+public class MainActivity extends SherlockFragmentActivity {
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	protected void onStop() {
+		super.onStop();
+		EasyTracker.getInstance().activityStart(this);
+	}
+
+	@Override
+	protected void onStart() {
+		super.onStart();
+		EasyTracker.getInstance().activityStop(this);
+	}
+}
