cc8d18df6c1d5a67e568af82edcdefffc3f93793
Aaswad Satpute <excellentmathematics@gmail.com>
2013-06-26 14:36:40 +0530

    AdMob

--- CricDeCode/src/co/acjs/cricdecode/MainActivity.java
+++ CricDeCode/src/co/acjs/cricdecode/MainActivity.java
@@ -1,16 +1,33 @@
 package co.acjs.cricdecode;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.os.Looper;
 
 import com.actionbarsherlock.app.SherlockFragmentActivity;
+import com.google.ads.AdRequest;
+import com.google.ads.AdView;
 import com.google.analytics.tracking.android.EasyTracker;
 
 public class MainActivity extends SherlockFragmentActivity {
+public static Context mainAct;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		// Main Activity Context
+		mainAct = this;
+
+		// Look up the AdView as a resource and load a request.
+		(new Thread() {
+			public void run() {
+				Looper.prepare();
+				AdView adView = (AdView) ((SherlockFragmentActivity) mainAct).findViewById(R.id.adView);
+				adView.loadAd(new AdRequest());
+			}
+		}).start();
 	}
 
 	@Override
