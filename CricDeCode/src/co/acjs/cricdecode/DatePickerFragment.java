6b937c9b43086b455382223ab2756041690f0a68
Saurabh Netravalkar <saurabh.netravalkar@gmail.com>
2013-06-28 13:53:52 +0530

    Commit

--- CricDeCode/src/co/acjs/cricdecode/DatePickerFragment.java
+++ CricDeCode/src/co/acjs/cricdecode/DatePickerFragment.java
@@ -19,9 +19,11 @@ public class DatePickerFragment extends SherlockDialogFragment implements
 	public Dialog onCreateDialog(Bundle savedInstanceState) {
 		// Use the current date as the default date in the picker
 		final Calendar c = Calendar.getInstance();
-		int year = c.get(Calendar.YEAR);
-		int month = c.get(Calendar.MONTH);
-		int day = c.get(Calendar.DAY_OF_MONTH);
+		int year, month, day;
+
+		year = c.get(Calendar.YEAR);
+		month = c.get(Calendar.MONTH);
+		day = c.get(Calendar.DAY_OF_MONTH);
 
 		// Create a new instance of DatePickerDialog and return it
 		return new DatePickerDialog(getActivity(), this, year, month, day);
