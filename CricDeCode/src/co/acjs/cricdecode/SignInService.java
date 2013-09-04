b00dce1af8c559c2b02e50ef39006e070471b038
Aaswad Satpute <excellentmathematics@gmail.com>
2013-09-04 23:41:54 +0530

    Incomplete but error solved

---
+++ CricDeCode/src/co/acjs/cricdecode/SignInService.java
@@ -0,0 +1,13 @@
+package co.acjs.cricdecode;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class SignInService extends Service{
+
+	@Override
+	public IBinder onBind(Intent intent) {
+		// TODO Auto-generated method stub
+		return null;
+	}}
