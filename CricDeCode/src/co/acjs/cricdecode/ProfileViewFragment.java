f9810de1d85e48c30a9331b6e18f271942d98e2d
Sheetal Pandrekar <sheetal@acjs.co>
2013-09-08 19:15:50 +0530

    UI - Profile View

--- CricDeCode/src/co/acjs/cricdecode/ProfileViewFragment.java
+++ CricDeCode/src/co/acjs/cricdecode/ProfileViewFragment.java
@@ -1,22 +1,29 @@
 package co.acjs.cricdecode;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Path;
+import android.graphics.Rect;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.actionbarsherlock.app.SherlockFragment;
 import com.facebook.widget.ProfilePictureView;
 
 public class ProfileViewFragment extends SherlockFragment {
-	static ProfileViewFragment	profileViewFragment;
-	ProfilePictureView			pro_pic;
+	static ProfileViewFragment profileViewFragment;
+	ProfilePictureView pro_pic;
+	ImageView pro_pic_real;
 
 	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
 		profileViewFragment = this;
 		View rootView = inflater.inflate(R.layout.profile_view, container,
 				false);
@@ -50,5 +57,27 @@ public class ProfileViewFragment extends SherlockFragment {
 				.setText(AccessSharedPrefs.mPrefs.getString("battingStyle", ""));
 		((TextView) view.findViewById(R.id.bowling_style))
 				.setText(AccessSharedPrefs.mPrefs.getString("bowlingStyle", ""));
+	}
+
+	public Bitmap getRoundedShape(Bitmap scaleBitmapImage) {
+		// TODO Auto-generated method stub
+		int targetWidth = 50;
+		int targetHeight = 50;
+		Bitmap targetBitmap = Bitmap.createBitmap(targetWidth, targetHeight,
+				Bitmap.Config.ARGB_8888);
+
+		Canvas canvas = new Canvas(targetBitmap);
+		Path path = new Path();
+		path.addCircle(((float) targetWidth - 1) / 2,
+				((float) targetHeight - 1) / 2,
+				(Math.min(((float) targetWidth), ((float) targetHeight)) / 2),
+				Path.Direction.CCW);
+
+		canvas.clipPath(path);
+		Bitmap sourceBitmap = scaleBitmapImage;
+		canvas.drawBitmap(sourceBitmap, new Rect(0, 0, sourceBitmap.getWidth(),
+				sourceBitmap.getHeight()), new Rect(0, 0, targetWidth,
+				targetHeight), null);
+		return targetBitmap;
 	}
 }
