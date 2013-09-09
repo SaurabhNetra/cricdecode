91b2a28b950afcb0e29142307b36e8e1c13b67a8
Aaswad Satpute <excellentmathematics@gmail.com>
2013-09-09 18:12:38 +0530

    tested GCM

--- CricDeCode_ServerSide/src/sync/GCmSend.php
+++ CricDeCode_ServerSide/src/sync/GCmSend.php
@@ -26,12 +26,13 @@ function SendGCm($SendToArr, $SendMsgArr, $id) {
 	$result = file_get_contents ( $urlToSend, false, $context );
 	$gcmj = json_decode ( $result );
 	for($i = 0; $i < count ( $gcmj->results ); $i ++) {
-		$eror = $gcmj->results [$i]->error;
-		if ($eror == "NotRegistered") {
-			$regiderr = $SendToArr [$i];
-			mysql_query ( "DELETE FROM " . TABLE_NAME . " WHERE " . GCM_ID_COLUMN_NAME . " = '$regiderr' AND " . USER_ID_COLUMN_NAME . " = '$id'" );
+		if (isset ( $gcmj->results [$i]->error )) {
+			if ($gcmj->results [$i]->error == "NotRegistered") {
+				$regiderr = $SendToArr [$i];
+				mysql_query ( "DELETE FROM " . TABLE_NAME . " WHERE " . GCM_ID_COLUMN_NAME . " = '$regiderr' AND " . USER_ID_COLUMN_NAME . " = '$id'" );
+			}
 		}
-		if (isset ( $gcmj->results [$i]->registration_id )) {
+		elseif (isset ( $gcmj->results [$i]->registration_id )) {
 			$newreg = $gcmj->results [$i]->registration_id;
 			$regiderr = $SendToArr [$i];
 			$result = mysql_query ( "SELECT * FROM " . TABLE_NAME . " WHERE " . GCM_ID_COLUMN_NAME . " = '$newreg' AND " . USER_ID_COLUMN_NAME . " = '$id'" );
