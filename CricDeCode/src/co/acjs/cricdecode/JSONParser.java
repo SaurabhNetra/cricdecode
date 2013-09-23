e53434a5f29357181a3314e0d4d873fafea62b15
Sheetal Pandrekar <sheetal@acjs.co>
2013-09-23 23:42:07 +0530

    Basic HTTP Authentication Android side

--- CricDeCode/src/co/acjs/cricdecode/JSONParser.java
+++ CricDeCode/src/co/acjs/cricdecode/JSONParser.java
@@ -1,9 +1,7 @@
 package co.acjs.cricdecode;
 
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
 import java.util.List;
 
@@ -11,17 +9,21 @@ import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.ResponseHandler;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.utils.URLEncodedUtils;
+import org.apache.http.impl.client.BasicResponseHandler;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.util.Base64;
 import android.util.Log;
 
 public class JSONParser {
@@ -30,6 +32,7 @@ public class JSONParser {
 	static JSONObject jObj = null;
 	static String json = "";
 	Context cont;
+	String response;
 
 	// constructor
 	public JSONParser() {
@@ -46,17 +49,20 @@ public class JSONParser {
 			this.cont = cont;
 			// check for request method
 			if (method == "POST") {
-				// request method is POST
-				// defaultHttpClient
+				
+				if(isOnline(cont)) {
+					Log.w("JSONParser","sending post request");
 				DefaultHttpClient httpClient = new DefaultHttpClient();
-				HttpPost httpPost = new HttpPost(url);
-				httpPost.setEntity(new UrlEncodedFormEntity(params));
-				if (isOnline(cont)) {
-					Log.w("Is Online", "JSONParser");
-					HttpResponse httpResponse = httpClient.execute(httpPost);
-					HttpEntity httpEntity = httpResponse.getEntity();
-					is = httpEntity.getContent();
-				} else {
+				ResponseHandler<String> resonseHandler = new BasicResponseHandler();
+				HttpPost postMethod = new HttpPost(url);
+			    postMethod.setEntity(new UrlEncodedFormEntity(params));
+				String authorizationString = "Basic " + Base64.encodeToString(("bhajji" + ":" + "__tube_l8").getBytes(), Base64.NO_WRAP); //this line is diffe
+				postMethod.setHeader("Authorization", authorizationString);
+			    response = httpClient.execute(postMethod,resonseHandler);
+				Log.w("HTTP respone","response: "+response);
+				}
+				else
+				{
 					Log.w("Not Online", "JSONParser");
 				}
 
@@ -72,6 +78,7 @@ public class JSONParser {
 
 					Log.w("Is Online", "JSONParser");
 					HttpResponse httpResponse = httpClient.execute(httpGet);
+					Log.d("Login: Response", EntityUtils.toString(httpResponse.getEntity()));
 					HttpEntity httpEntity = httpResponse.getEntity();
 					is = httpEntity.getContent();
 				} else {
@@ -87,7 +94,7 @@ public class JSONParser {
 			e.printStackTrace();
 		}
 
-		try {
+	/*	try {
 			BufferedReader reader = new BufferedReader(new InputStreamReader(
 					is, "iso-8859-1"), 8);
 			StringBuilder sb = new StringBuilder();
@@ -107,10 +114,17 @@ public class JSONParser {
 		} catch (JSONException e) {
 			Log.e("JSON Parser", "Error parsing data " + e.toString());
 		}
+		*/
 
 		// return JSON String
-		return jObj;
-
+		JSONObject k=null;
+		try {
+			k=new JSONObject(response);
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return k;
 	}
 
 	public Boolean isOnline(Context cont) {
