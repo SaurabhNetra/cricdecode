6708bacc81057b232c3742b3abbbb8ff4cf21b9a
Aaswad Satpute <excellentmathematics@gmail.com>
2013-10-16 18:37:28 +0530

    GCMMigrating

--- CricDeCode_ServerSide/src/sync/getAPIkEY.php
+++
@@ -1,4 +0,0 @@
-<?
-define ( 'API_KEY', 'AIzaSyARgoalVf6Acm9ZImdegEwqPTB-1cQCisk' );
-define ( 'APP_NAME', 'cricdecode' );
-?>
--- CricDeCode_ServerSide/src/sync/get_Consts.php
+++
@@ -1,5 +0,0 @@
-<?php
-define ( 'TABLE_NAME', 'user_android_devices' );
-define ( 'GCM_ID_COLUMN_NAME', 'gcm_id' );
-define ( 'USER_ID_COLUMN_NAME', 'id' );
-?>
